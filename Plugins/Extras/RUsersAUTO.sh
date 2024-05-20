f95ddd6d5344605c4b523c3b0e784de4ec2c2b21
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2024-05-20 16:08:27 -0500

    AutoRespaldos SSH

--- Plugins/Extras/RUsersAUTO.sh
+++ Plugins/Extras/RUsersAUTO.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 _Key='/etc/cghkey'
-
+[[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] && apt purge apache2 -y &>/dev/null
 clear
 _SFTP="$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN" | grep apache2)"
 [[ -z ${_SFTP} ]] && _SFTP="$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN" | grep nginx)"
@@ -10,9 +10,11 @@ portFTP=$(echo ${portFTP} | sed 's/\s\+/,/g' | cut -d , -f1)
 
 [[ ! -e ${_Key} ]] && exit 
 
+[[ -e /bin/ejecutar/autt ]] || exit
 dir_user="/userDIR"
 dir="/etc/adm-lite"
 name=$(cat < /bin/ejecutar/autt)
+
 bc="$HOME/$name"
 arquivo_move="$name"
 fun_ip () {
@@ -69,8 +71,10 @@ echo -e "\033[1;37mHaciendo Backup de Usuarios...\033[0m"
 for user in `cat "/etc/passwd"|grep 'home'|grep 'false'|grep -v 'syslog' | cut -d: -f1 |sort`
 do
 [[ -e $dir$dir_user/$user ]] && {
-pass=$(cat $dir$dir_user/$user | grep "senha" | awk '{print $2}')
+####################VALIDACION DE SCRIPT#####################
+pass="$(cat $dir$dir_user/$user | grep "senha" | awk '{print $2}')"
 limite=$(cat $dir$dir_user/$user | grep "limite" | awk '{print $2}')
+[[ $limite = @(HWID|TOKEN) ]] && NameTKID=${pass} || NameTKID=${pass}
 data=$(cat $dir$dir_user/$user | grep "data" | awk '{print $2}')
 data_sec=$(date +%s)
 data_user=$(chage -l "$user" |grep -i co |awk -F ":" '{print $2}')
@@ -85,33 +89,30 @@ sl=$((dias_use + 1))
 i=$((i + 1))
 [[ -z "$limite" ]] && limite="5"
 echo -e "\033[1;31m [ SCRIPT ] \033[1;37m "
+####################VALIDACION DE SCRIPT#####################
 } || {
+####################VALIDACION DE PASSWD#####################
 linea=$(cat /etc/passwd | grep -w ${user})
-if [[ "${linea}" =~ ,([^:]+): ]]; then
-        pass="${BASH_REMATCH[1]}"
-fi
 limite="$(cat /etc/passwd | grep -w ${user} | awk -F ':' '{split($5, a, ","); print a[1]}')"
+if [[ "${linea}" =~ ,([^:]+): ]]; then
+        NameTKID="${BASH_REMATCH[1]}"
+fi
+[[ -z "$limite" ]] && limite="5"
 data_sec=$(date +%s)
 data_user=$(chage -l "$user" |grep -i co |awk -F ":" '{print $2}')
 data_user_sec=$(date +%s --date="$data_user")
 variavel_soma=$(($data_user_sec - $data_sec))
 dias_use=$(($variavel_soma / 86400))
-if [[ "$dias_use" -le 0 ]]; 
-then
-dias_use=0
-fi
+[[ "$dias_use" -le 0 ]] && dias_use=0
 sl=$((dias_use + 1))
 i=$((i + 1))
-[[ -z "$limite" ]] && limite="5"
-echo -ne "\033[1;31m [ SYSTEM ] \033[1;37m"
-#read -p "Introduzca la contraseña manualmente o pulse ENTER: " pass
- if [ -z "$pass" ]; then
-pass="$user"
- fi
+echo -e "\033[1;31m [ SYSTEM ] \033[1;37m"
+####################VALIDACION DE PASSWD#####################
 }
-[[ ${limite} = "HWID" ]] && echo "$user:$user:HWID:$sl:$pass" >> $bc && echo -e "\033[1;37mUser $pass \033[0;35m [\033[0;36m$limite\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
-[[ ${limite} = "TOKEN" ]] && echo "$user:$passTK:TOKEN:$sl:$pass" >> $bc && echo -e "\033[1;37mUser $pass \033[0;35m [\033[0;36m$limite\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
-[[ "$limite" =~ ^[0-9]+$ ]] && echo "$user:$pass:$limite:$sl" >> $bc && echo -e "\033[1;37mUser $user \033[0;35m [\033[0;36mSSH\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
+[[ $(echo $limite) = "HWID" ]] && echo "$user:$user:HWID:$sl:$NameTKID" >> $bc && echo -e "\033[1;37mUser $NameTKID \033[0;35m [\033[0;36m$limite\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
+[[ $(echo $limite) = "TOKEN" ]] && echo "$user:$passTK:TOKEN:$sl:$NameTKID" >> $bc && echo -e "\033[1;37mUser $NameTKID \033[0;35m [\033[0;36m$limite\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
+[[ "$limite" =~ ^[0-9]+$ ]] && echo "$user:$NameTKID:$limite:$sl:$NameTKID" >> $bc && echo -e "\033[1;37mUser $user \033[0;35m [\033[0;36mSSH\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
+#sleep .2s
 done
 echo " "
 echo -e "\033[1;31mBackup Completado !!!\033[0m"
